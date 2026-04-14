9e897f81330556dc986ac7a5a430f4d41a6da816
Parth <parthagre84@gmail.com>
2026-04-14 21:20:27 +0530

    adding till customer and admin login and regfistration and minor changes

--- src/customer_dashboard.java
+++ src/customer_dashboard.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 public class customer_dashboard extends JFrame{
     JLabel l;
-    admin_dashboard(){
+    customer_dashboard(){
         l = new JLabel("Customer Dashboard");
         setLayout(new FlowLayout());
         add(l);
